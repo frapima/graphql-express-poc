0a9c5224ab7bd49b6d311bf415d8a327f196daec
frapima <51753282+frapima@users.noreply.github.com>
2019-06-12 16:59:27 +0200

    Add files via upload

---
+++ project/server/serverThird.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
+  
+    type Query {
+        getDie(numSides: Int): RandomDie
+    }
+`);
+
+class RandomDie {
+    constructor(numSides) {
+        this.numSides = numSides;
+    }
+
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+
+    roll({ numRolls }) {
+        var output = [];
+        for (var i = 0; i < numRolls; i++) {
+            output.push(this.rollOnce());
+        }
+        return output;
+    }
+}
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    getDie: ({numSides}) => {
+        return new RandomDie(numSides || 6);
+    },
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    },
+    rollDice: (args) => {
+        let output = [];
+        for (let i = 0; i < args.numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+        }
+        return output;
+    }
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
